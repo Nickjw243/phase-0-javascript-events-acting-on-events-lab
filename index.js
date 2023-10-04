3f65fa32c8bb94b8a35688e186b1b9746902224d
Nick Weiland <njweiland23@gmail.com>
2023-10-04 11:00:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function moveDodgerRight() {
     const left = parseInt(leftNumbers, 10);
 
     if (left < 360) {
-        dodger.style.left = `${left + 1}px`
+      dodger.style.left = `${left + 1}px`
     }
 }
 document.addEventListener("keydown", function (e) {
